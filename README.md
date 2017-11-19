49db543f530d054561cdaae120052684e0f10df3
hodongman <arshavin3@naver.com>
2017-11-20 00:06:29 +0900

    youtube library update

---
+++ README.md
@@ -0,0 +1,8 @@
+YOUTUBE API를 활용해서 사용자가 발생시키는 이벤트를 통해 데이터를 수집
+
+sklearn의 classification => http://scikit-learn.org/stable/modules/svm.html#classification을 활용한 필요성이 있어보임
+
+Front-end : React / Webpack (Scaffolding : create-react-app)
+Back-end : Network I/O (Node.js or Django rest-framework)
+         : Cassification (Django rest-framework or wgi dispatcher / sklearn)
+
---
+++ videoclient/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+
+class App extends Component {
+
+    printState(event){
+        console.log(event.target);
+        console.log(event.target.getCurrentTime());
+        console.log(event.target.getDuration());
+    }
+
+    onReady(event){
+        event.target.pauseVideo();
+    }
+
+    render() {
+
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 1
+            }
+        };
+
+        return (
+            <YouTube
+                videoId="gQTQxEp7OLc"
+                opts={opts}
+                onReady={this.onReady}
+                onPlay={this.printState}                     // defaults -> noop
+                onPause={this.printState}                    // defaults -> noop
+                onEnd={this.printState}                      // defaults -> noop
+                onError={this.printState}                    // defaults -> noop
+                onStateChange={this.printState}              // defaults -> noop
+                onPlaybackRateChange={this.printState}       // defaults -> noop
+                onPlaybackQualityChange={this.printState}
+            />
+        );
+    }
+}
+
+export default App;
